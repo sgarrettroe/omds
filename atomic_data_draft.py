0730ebe779f807bd2a3a0de756764558a7c7c00a
Sean Garrett-Roe <sgarrettroe@gmail.com>
2024-02-10 10:20:38 -0500

    fix(atomicdata): Change logging to basename of file

--- atomic_data_draft.py
+++ atomic_data_draft.py
@@ -21,7 +21,7 @@ logging.basicConfig(
     level=logging.DEBUG,
     format='%(asctime)s %(name)-24s %(levelname)-8s %(message)s'
 )
-logger = logging.getLogger('omds.atomic_data_draft')
+logger = logging.getLogger(os.path.basename(__file__))
 logger.setLevel(logging.DEBUG)
 
 
